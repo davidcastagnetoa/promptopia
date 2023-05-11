ee28855e222390a1ba34d378c8c9b2eb77b18dd9
David Castagneto A <davidcastagnetoa@gmail.com>
2023-05-11 03:21:30 +0200

    create-prompt issue resolved

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,17 @@
+import Prompt from "@models/prompt";
+import { connectToDatabase } from "@utils/database";
+
+export const POST = async (request) => {
+  const { userId, prompt, tag } = await request.json();
+  try {
+    await connectToDatabase();
+    const newPrompt = new Prompt({ creator: userId, prompt, tag });
+    await newPrompt.save();
+    return new Response(JSON.stringify(newPrompt), { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return new Response(error.message || "Failed to create a new prompt", {
+      status: 500,
+    });
+  }
+};
